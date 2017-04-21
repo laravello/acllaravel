84d66d77340e2262eacae1578fe6df91f6794271
magnus32 <bruemmersven@gmx.de>
2017-04-21 16:08:13 +0200

    'test'

---
+++ resources/lang/de/acl.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'username'   => 'Benutzername',
+    'desiredusername'   => 'gewünschter Benutzername',
+    'email' => 'E-Mail',
+    'password'   => 'Passwort',
+    'register'   => 'Registrieren',
+    'login'   => 'Einloggen',
+    'confirmpassword'   => 'Passwort bestätigen',
+    'login'   => 'Login',
+    'forgotpassword'   => 'Passwort vergessen',
+    'rememberme'   => 'Passwort merken',
+
+];
