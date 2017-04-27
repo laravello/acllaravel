1444e46f66cf6d02b15034609630931aa128ad39
laravello <s.bruemmer@neusta-grafenstein.de>
2017-04-27 15:00:57 +0200

    'chapter7'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,6 +60,19 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
+        $guard = array_get($exception->guards(),0);
+
+        switch ($guard){
+            case 'admin':
+                return redirect()->guest(route('admin.login'));
+                break;
+
+            default:
+                return redirect()->guest(route('login'));
+                break;
+
+        }
+
         return redirect()->guest(route('login'));
     }
 }
